e3ceee662c7dfe997c23822321d9807b87bf6cd4
Jens Korinth <korinth@wingcopter.com>
2026-03-20 09:55:11 +0100

    feat(rust): add rust-dev shell

--- flake.nix
+++ flake.nix
@@ -237,6 +237,15 @@
               typstyle
             ];
           };
+
+	  rust-dev = pkgs.mkShell {
+	    name = "rust-dev";
+	    packages = with pkgs; [
+	      openssl
+	      pkg-config
+	      rust-bin.beta.latest.default
+	    ];
+	  };
         };
       }
     );
